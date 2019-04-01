db2e87e4c99fc4578f7eb8bc0b8b0976f132a6b6
s_jqzhang <s_jqzhang@163.com>
2019-04-01 10:00:32 +0800

    bugfix cross domain access and modify api doc

--- fileserver.go
+++ fileserver.go
@@ -1193,11 +1193,13 @@ func (this *Server) DownloadFromPeer(peer string, fileInfo *FileInfo) {
 		this.SaveFileMd5Log(fileInfo, CONST_FILE_Md5_FILE_NAME)
 	}
 }
-
 func (this *Server) CrossOrigin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, X-File-Type, Cache-Control, Origin")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
+	//https://blog.csdn.net/yanzisu_congcong/article/details/80552155
 }
-
 func (this *Server) SetDownloadHeader(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Header().Set("Content-Disposition", "attachment")
@@ -1273,7 +1275,7 @@ func (this *Server) Download(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	code = r.FormValue("code")
-	isDownload=true
+	isDownload = true
 	if r.FormValue("download") == "" {
 		isDownload = Config().DefaultDownload
 	}
