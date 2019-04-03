593b14b07dfbad4ac9d49503c4c1e00d556172ed
s_jqzhang <s_jqzhang@163.com>
2019-04-03 09:59:34 +0800

    modify benchmark description

--- gen_file.py
+++ gen_file.py
@@ -4,6 +4,6 @@ j=0
 for i in range(0,1000000):
     if i%1000==0:
         j=i
-        os.system('mkdir %s'%(i))
+        os.system('mkdir -p %s'%(i))
     with open('%s/%s.txt'%(j,i),'w+') as f:
         f.write(str(i))
