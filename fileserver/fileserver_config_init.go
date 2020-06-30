59993d4c7d24ea34779ed75a53e758e517375baf
gavin <gongy@sunwayworld.com>
2020-06-30 09:30:33 +0800

    增加获取初始化配置的go文件，通过环境变量获取如下配置： * ：GO_FASTDFS_PORT，如果没有默认为8080； * ：GO_FASTDFS_PEER_ID，如果没有默认原来方式获取一个随机值； * ：基于构造，如果没有默认原来方式获取。 * ：GO_FASTDFS_PEERS，如果没有默认原来处理方式； * ：GO_FASTDFS_GROUP，如果没有默认group1； 调整代码结构。

---
+++ fileserver/fileserver_config_init.go
@@ -0,0 +1,52 @@
+package fileserver
+
+import (
+	"fmt"
+	"os"
+)
+
+/***
+通过环境变量来初始化默认配置，例如
+GO_FASTDFS_PEER_ID=1;GO_FASTDFS_PORT="8080";GO_FASTDFS_IP=tomcat.gavin.com;GO_FASTDFS_PEERS="http://tomcat.gavin.com:8080";GO_FASTDFS_PORT=8080;GO_FASTDFS_GROUP=group1;
+***/
+
+func (server *Server) getInitPeerId() string{
+	var peerId string
+	if peerId = os.Getenv("GO_FASTDFS_PEER_ID"); peerId == "" {
+		peerId = fmt.Sprintf("%d", server.util.RandInt(0, 9))
+	}
+	return peerId
+}
+
+func (server *Server) getInitPort() string{
+	var port string
+	if port = os.Getenv("GO_FASTDFS_PORT"); port == "" {
+		port = "8080"
+	}
+	return port
+}
+
+func (server *Server) getInitHost() string{
+	var ip string
+	if ip = os.Getenv("GO_FASTDFS_IP"); ip == "" {
+		ip = server.util.GetPulicIP()
+	}
+	peer := "http://" + ip + ":" + server.getInitPort()
+	return peer;
+}
+
+func (server *Server) getInitPeers() string {
+	var peers string
+	if peers = os.Getenv("GO_FASTDFS_PEERS"); peers == "" {
+		peers = "\"" + server.getInitHost() + "\""
+	}
+	return peers
+}
+
+func (server *Server) getInitGroup() string{
+	var group string
+	if group = os.Getenv("GO_FASTDFS_GROUP"); group == "" {
+		group = "group1"
+	}
+	return group;
+}
--- fileserver_test.go
+++ fileserver_test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/astaxie/beego/httplib"
 	"github.com/eventials/go-tus"
 	"github.com/sjqzhang/goutil"
+	"go-fastdfs/fileserver"
 	"io/ioutil"
 	_ "net/http/pprof"
 	"os"
@@ -24,7 +26,7 @@ var testUtil = goutil.Common{}
 var endPoint = "http://127.0.0.1:8080/group1"
 var endPoint2 = ""
 
-var testCfg *GloablConfig
+var testCfg *fileserver.GloablConfig
 
 var testSmallFileMd5 = ""
 var testBigFileMd5 = ""
@@ -102,7 +104,7 @@ func uploadContinueBig(t *testing.T) {
 
 func refreshConfig(t *testing.T) {
 	var (
-		cfg    GloablConfig
+		cfg    fileserver.GloablConfig
 		err    error
 		cfgStr string
 		result string
@@ -138,11 +140,11 @@ func refreshConfig(t *testing.T) {
 func testConfig(t *testing.T) {
 
 	var (
-		cfg        GloablConfig
+		cfg        fileserver.GloablConfig
 		err        error
 		cfgStr     string
 		result     string
-		jsonResult JsonResult
+		jsonResult fileserver.JsonResult
 	)
 
 	req := httplib.Get(endPoint + "/reload?action=get")
@@ -248,10 +250,10 @@ func testApis(t *testing.T) {
 	)
 
 	apis := []string{"/index","/status", "/stat", "/repair?force=1", "/repair_stat",
-	"/sync?force=1&date=" + testUtil.GetToDay(),"/delete?md5="+testSmallFileMd5,
-	"/repair_fileinfo","","/list_dir","/gen_google_code?secret=N7IET373HB2C5M6D",
-	"/gen_google_secret","/receive_md5s?md5s=xx","/remove_empty_dir","/backup","/search?kw=ab",
-	"/reload=get","/back","/report"}
+		"/sync?force=1&date=" + testUtil.GetToDay(),"/delete?md5="+ testSmallFileMd5,
+		"/repair_fileinfo","","/list_dir","/gen_google_code?secret=N7IET373HB2C5M6D",
+		"/gen_google_secret","/receive_md5s?md5s=xx","/remove_empty_dir","/backup","/search?kw=ab",
+		"/reload=get","/back","/report"}
 	for _, v := range apis {
 		req := httplib.Get(endPoint + v)
 		req.SetTimeout(time.Second*2, time.Second*3)
@@ -303,7 +305,7 @@ func uploadContinueSmall(t *testing.T) {
 }
 
 func uploadSmall(t *testing.T) {
-	var obj FileResult
+	var obj fileserver.FileResult
 	req := httplib.Post(endPoint + "/upload")
 	req.PostFile("file", CONST_SMALL_FILE_NAME)
 	req.Param("output", "json")
@@ -323,7 +325,7 @@ func uploadSmall(t *testing.T) {
 }
 
 func uploadLarge(t *testing.T) {
-	var obj FileResult
+	var obj fileserver.FileResult
 	req := httplib.Post(endPoint + "/upload")
 	req.PostFile("file", CONST_BIG_FILE_NAME)
 	req.Param("output", "json")
@@ -344,7 +346,7 @@ func uploadLarge(t *testing.T) {
 }
 
 func checkFileExist(t *testing.T) {
-	var obj FileInfo
+	var obj fileserver.FileInfo
 	req := httplib.Post(endPoint + "/check_file_exist")
 	req.Param("md5", testBigFileMd5)
 	req.ToJSON(&obj)
@@ -384,9 +386,9 @@ func Test_main(t *testing.T) {
 			uploadLarge(t)
 			checkFileExist(t)
 			testApis(t)
-			if endPoint != endPoint2 && endPoint2!="" {
+			if endPoint != endPoint2 && endPoint2 !="" {
 				endPoint = endPoint2
-				fmt.Println("#######endPoint2######",endPoint2)
+				fmt.Println("#######endPoint2######", endPoint2)
 				initFile(1024*testUtil.RandInt(100, 512), 1024*1024*testUtil.RandInt(2, 20))
 				uploadContinueBig(t)
 				uploadContinueSmall(t)
